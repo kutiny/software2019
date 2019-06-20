9695ba7c66cda60f225ebfc03fcb99645c025f9a
Juanchula <jruloignacio@hotmail.com>
2019-06-20 19:50:06 -0300

    Desarrollo de GUI

---
+++ src/main/java/start/ControllerLogin.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author alexi
+ */
+public class ControllerLogin implements Initializable {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+
+    @FXML
+    private void firstClick(ActionEvent event){
+        label.setText("has clickiado perro");
+        System.out.println("Has hecho click");
+//        try {
+//        	AnchorPane pane = FXMLLoader.load(getClass().getResource("Game.fxml"));
+//        	rootPane.getC
+//
+//        }catch(Exception e) {
+//        	return;
+//        }
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+
+            Scene escena = new Scene(root);
+            stage.setScene(escena);
+            stage.show();
+            ( (Node) (event.getSource() ) ).getScene().getWindow().hide();
+        }
+        catch(IOException e){
+            System.out.println("La cague");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/main/java/start/Start.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start  ;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author alexi
+ */
+public class Start extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+
+//        Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 1280, 720));
+//        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
