4cf42ae5eff82e5b21e5069347c1f7bb2ad4601d
AixaCarrizo <aixa.carrizo1704@gmail.com>
2019-06-20 22:26:28 -0300

    fixed warrior class

--- src/main/java/game/Archer.java
+++ src/main/java/game/Archer.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Archer extends CharacterClass {
 	  public Archer(){
 		  this.className="Archer";
-//		  this.skills.add(new ArcherSkill1());
-//		  this.skills.add(new ArcherSkill2());
-//		  this.skills.add(new ArcherSkill3());
+		  this.skills= new ArrayList<Skill>();
+		  this.skills.add(new ArcherSkill1());
+		  this.skills.add(new ArcherSkill2());
+		  this.skills.add(new ArcherSkill3());
 		  activeSkill=null;
 		  maxHealth=200;
 		  armor=150;
