abdbb313e2ae80ee4ad9e2273defc1473817b359
Juanchula <jruloignacio@hotmail.com>
2019-06-22 15:27:33 -0300

    Cambios sobre GUI, agregado y configurado Login

--- src/main/java/start/Start.java
+++ src/main/java/start/Start.java
@@ -21,7 +21,7 @@ public class Start extends Application {
     @Override
     public void start(Stage stage) throws Exception {
     	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("/start/Login2.fxml"));
+    		Parent root = FXMLLoader.load(getClass().getResource("/start/Login.fxml"));
             Scene scene = new Scene(root);
 
             stage.setScene(scene);
