63be67254c746f992e1a6d29cf4fe0ef66559d31
alexlndn <alex08lta@gmail.com>
2019-06-23 20:53:09 -0300

    refactorizacion

--- src/main/java/game/MageSkill1.java
+++ src/main/java/game/MageSkill1.java
@@ -1,27 +1,26 @@
 package game;
 
 public class MageSkill1 extends Skill{
-public MageSkill1() {
-	  this.skillName = "Winter's Grasp";
-	  this.critProbability = 0.15;
-	  this.failProbability = 0.2;
-	  this.damage= new Damage(new DamageType("magical"), 53, 53);
-	  }
+	public MageSkill1() {
+		this.skillName = "Winter's Grasp";
+		this.critProbability = 0.15;
+		this.failProbability = 0.2;
+		this.damage= new Damage(new DamageType("magical"), 53, 53);
+	}
 
 
-@Override
-public Damage use() {
-	// TODO Auto-generated method stub
-	if( (this.failProbability+Math.random())<0.5) {
-		damage.setDamagePoints(0);
+	@Override
+	public Damage use() {
+		if( (this.failProbability + Math.random()) < MEDIUMPROB) {
+			damage.setDamagePoints(0);
+		}
+		else if (this.critProbability + Math.random() < MEDIUMPROB) {
+			damage.setDamagePoints(damage.getBasicDamagePoints() * 2);
+		}
+		else {
+			damage.setDamagePoints(damage.getBasicDamagePoints());
+		}
+		return damage;
 	}
-	else if (this.critProbability+Math.random()<0.5) {
-		damage.setDamagePoints(damage.getBasicDamagePoints()*2);
-	}
-	else {
-		damage.setDamagePoints(damage.getBasicDamagePoints());
-	}
-	return damage;
-}
 }
 
