54f6a03d30bcd22f105e7b55d3f680e42aec11e9
AixaCarrizo <aixa.carrizo1704@gmail.com>
2019-06-23 17:06:37 -0300

    duel class fixed

--- src/main/java/game/Duel.java
+++ src/main/java/game/Duel.java
@@ -4,32 +4,14 @@ public class Duel {
 	private Character character;
 	private Enemy enemy;
 	private boolean isCharacterNext;
-	private boolean isEnemyAlive;
-	private boolean isCharacterAlive;
+
 	
 	public Duel(Character c, Enemy e) {
 		this.character = c;
 		this.enemy = e;
 		this.isCharacterNext = true;
-		this.isCharacterAlive=true;
-		this.isEnemyAlive=true;
+
 	}
-	
-	public void fight() {
-		while (this.isEnemyAlive&&this.isCharacterAlive) {
-		   if(this.characterAttack(character.getCharaClass().getActiveSkill())) {
-			   this.isCharacterAlive= this.EnemyAttack();
-		   }
-		   else {
-			   this.isEnemyAlive=false;
-			   break;
-		   }
-		}
-		if(this.isCharacterAlive) {
-			character.addExp(this.enemy.getExpKilled());
-		}
-	}
-	
 	
 	public boolean characterAttack(Skill skill) throws IllegalStateException {
 		if(this.isCharacterNext) {
@@ -37,24 +19,28 @@ public class Duel {
 
 			this.character.getCharaClass().setActiveSkill(skill);
 			this.enemy.receiveDamage(this.enemy.calculateRecievedDamage(this.character.fight()));
-			if(enemy.getHp()>0)
-			return true;
-			else return false;
-			
+				if(enemy.getHp()>0)
+				return true;
+				else {
+						character.addExp(this.enemy.getExpKilled());
+					return false;
+				}
 		}else {
 			throw new IllegalStateException("No puedes atacar durante el turno del enemigo");
 		}
 		
 	}
-	public boolean EnemyAttack(){
+	public boolean EnemyAttack() throws IllegalStateException{
 		if(!this.isCharacterNext) {
 			this.isCharacterNext = true;
 			
 			this.character.receiveDamage(this.character.getCharaClass().calculateRecievedDamage(this.enemy.fight()));
-			return true;
-			
-		}else {
-			throw new IllegalStateException("No puedes atacar durante el turno del enemigo");
+			if(character.getHp()>0) 
+				return true;
+			else return false;
+		}
+		else {
+			throw new IllegalStateException("Enemigo atacando en turno de personaje");
 		}
 		
 	}
