3ae2dedb746d6466cedb37d6b8c80e2314816dcd
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-11-06 08:46:01 -0300

    Observers tests

--- src/test/java/observer/ObserverTest.java
+++ src/test/java/observer/ObserverTest.java
@@ -8,23 +8,39 @@ import org.junit.Test;
 public class ObserverTest {
 	CharacterLevelObserver clo;
 	CharacterLifeObserver clife;
-	
+
 	@Before
 	public void ExecuteBeforeEach() {
 		this.clo = new CharacterLevelObserver();
 		this.clife = new CharacterLifeObserver();
 	}
-	
+
 	@Test
 	public void notifyLevelTest() {
 		this.clo.notifyHandler(3);
 		assertEquals(3, this.clo.getLastValue());
 	}
-	
+
 	@Test
 	public void notifyLifeTest() {
 		this.clife.notifyHandler(3);
 		assertEquals(3, this.clife.getLastValue());
 	}
-	
+
+	@Test
+	public void lifeNotifyHandlerErrorTest() {
+		int val = 3;
+		this.clife.notifyHandler(val);
+		this.clife.notifyHandler(null);
+		assertEquals(val, this.clife.getLastValue());
+	}
+
+	@Test
+	public void levelNotifyHandlerErrorTest() {
+		int val = 3;
+		this.clo.notifyHandler(val);
+		this.clo.notifyHandler("test");
+		assertEquals(val, this.clo.getLastValue());
+	}
+
 }
